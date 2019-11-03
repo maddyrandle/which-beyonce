795678fd9bd3d74741fdb698ddebf95b041a4674
Madison Randle <madisonrandle25@gmail.com>
2019-11-03 05:35:54 -0700

    Add divs for each card

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
 var playerForm = document.querySelector(".player-form");
-var firstPlayGameButton = document.querySelector(".button");
-var secondPlayGameButton = document.querySelector(".button");
+var playGameButton = document.querySelector(".button");
+var instructionPlayGameButton = document.querySelector(".button-2");
 var playerOneName = document.querySelector(".input-1");
 var playerTwoName = document.querySelector(".input-2");
 var instructionPage = document.querySelector(".instruction-page");
 var gameboardPage = document.querySelector(".gameboard-container");
 
-firstPlayGameButton.addEventListener("click", checkNameInput);
-secondPlayGameButton.addEventListener("click", showGameboard)
-
+playGameButton.addEventListener("click", checkNameInput);
+playGameButton.addEventListener("click", showGameboard);
 
 function checkNameInput() {
   if (playerOneName.value.length > 0) {
     hidePlayerForm();
     showInstructions();
   } else {
-    alert("ENTER PLAYER NAME(S) TO CONTINUE!");
+    alert("ENTER PLAYER NAME(S) TO CONTINUE");
   }
 }
 
@@ -27,17 +26,15 @@ function hideInstructionPage() {
   instructionPage.style.display = "none";
 }
 
-function showInstructions(name) {
+function showInstructions() {
   var instructionPageHtml = `
-    <div class="instruction-page">
-      <div class="instruction-box">
-        <h2>WELCOME <span class="player">${playerOneName.value}</span>!</h2>
-        <p class="instruction">The goal of the game is to find all 5 pairs of cards as quickly as possible. The player that finds the greatest numbers of pairs, wins.</p>
-        <p class="instruction">To begin playing, the player whose name is highlighted can click any card in the card pile. It will flip over and reveal a picture of Beyoncé. Click another card. If they match, they will disappear and you will have completed a match! If they don’t, you’ll have three seconds to look at them before they flip back over. Then it’s time for the other player to try!</p>
-        <p class="instruction">After you play, you’ll see the name of the final winner and how long it took to win the game.</p>
-      </div>
-      <button class="button" type="button" name="button">PLAY GAME</button>
+    <div class="instruction-box">
+      <h2>WELCOME <span class="player">${playerOneName.value}</span>!</h2>
+      <p class="instruction">The goal of the game is to find all 5 pairs of cards as quickly as possible. The player that finds the greatest numbers of pairs, wins.</p>
+      <p class="instruction">To begin playing, the player whose name is highlighted can click any card in the card pile. It will flip over and reveal a picture of Beyoncé. Click another card. If they match, they will disappear and you will have completed a match! If they don’t, you’ll have three seconds to look at them before they flip back over. Then it’s time for the other player to try!</p>
+      <p class="instruction">After you play, you’ll see the name of the final winner and how long it took to win the game.</p>
     </div>
+    <button class="button button-2" type="button" name="button">PLAY GAME</button>
   `;
       instructionPage.insertAdjacentHTML("afterbegin", instructionPageHtml);
 }
